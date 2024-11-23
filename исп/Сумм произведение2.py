201824ee86c4e33e2bf2cba6ffb15b1fd50d88bc
Ahmad56788765 <ahmadustarhanov77@gmail.com>
2024-11-23 22:01:44 +1000

    Add files via upload

---
+++ исп/Сумм произведение2.py
@@ -0,0 +1,12 @@
+# TODO решите задачу
+import json
+input1 = "input.json"
+def task() -> float:
+    with open(input1, encoding='utf-8') as f:
+        x = json.load(f)
+        t =[i["score"]*i["weight"] for i in x]
+    return round(sum(t), 3)
+
+
+
+print(task())
